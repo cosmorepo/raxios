50ae49e823f6c7ea254e8fc12a0ff62cd3efb534
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2021-08-26 04:12:21 -0700

    added pagination

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,21 +1,39 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import _ from 'lodash'
+
+
+const pageSize = 10
+
 
 const Posts = () => {
 
     const [posts, setPosts] = useState()
+    const [paginatedPosts, setpaginatedPosts] = useState()
+    const [currentPage, setcurrentPage] = useState(1)
 
     useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/todos').then(res=>{
-            console.log(res.data);
+            //console.log(res.data);
             setPosts(res.data)
+            setpaginatedPosts(_(res.data).slice(0).take(pageSize).value())
         })
-    })
+    }, [])
+
+    const pageCount = posts? Math.ceil(posts.length/pageSize) : 0
+    if(pageCount === 1) return null
+    const pages = _.range(1, pageCount+1)
+    const pagination=(pageNo)=>{
+        setcurrentPage(pageNo)
+        const startIndex = (pageNo - 1)*pageSize
+        const paginatedPost = _(posts).slice(startIndex).take(pageSize).value()
+        setpaginatedPosts(paginatedPost)
+    }
 
     return (
         <div>
             {
-             !posts ? ("we do not have any data") : (
+             !paginatedPosts ? ("we do not have any data") : (
                 <table className='table'>
                     <thead>
                         <tr>
@@ -26,7 +44,7 @@ const Posts = () => {
                         </tr>
                     </thead>
                     <tbody>{
-                        posts.map((post, index)=> (
+                        paginatedPosts.map((post, index)=> (
                             <tr key={index}>
                                 <td>{post.id}</td>
                                 <td>{post.userId}</td>
@@ -42,6 +60,25 @@ const Posts = () => {
                 </table>
             )
              }
+             <nav className="d-flex justify-content-center">
+                <ul className="pagination">
+                    {
+                        pages.map((page, index)=>(
+                            <li key={index} className={
+                                page === currentPage ? "page-item active" : "page-item"
+                            }>
+                                <p 
+                                    className="page-link"
+                                    onClick={()=>pagination(page)}
+                                >{page}</p>
+                            </li>
+                        ))
+                    }
+                    
+                   
+
+                </ul>
+             </nav>
         </div>
     )
 }
